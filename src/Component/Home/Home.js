f8b80d31dbc1780969011206c6642f1f33b3377c
Ahmed Rifat <rifatbafsc@gmail.com>
2022-07-29 10:18:34 +0600

    home page position fixing

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -6,16 +6,16 @@ import "./Home.css"
 const Home = () => {
     return (
         <div className='container row'>
-            <div className='col-6'>
-            <h1>Welcome to Globtech</h1>
-            <p>We are committed to deliver <span>best IT services</span>. Our Consultants have experience in working with clients. We have extensive experience in the software application space and also offer a broad range and depth of technology.</p>
+            <div className='col-8'>
+            <h1 className='welcomeText'>Welcome to <span>Globtech</span></h1>
+            <p className='itService'>We are committed to deliver <span>best IT services</span>. Our Consultants have experience in working with clients. We have extensive experience in the software application space and also offer a broad range and depth of technology.</p>
             <div>
                 <Button>Support us</Button>
                 <Button>Our Mission</Button>
             </div>
             </div>
 
-            <div className='col-6'>
+            <div className='col-4'>
                 <img className='ellipse' src={bannerImg} alt="" />
             </div>
         </div>
