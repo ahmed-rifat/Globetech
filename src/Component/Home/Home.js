9a0bd4c577fc872cb8a42724bfcae943d547608b
Ahmed Rifat <rifatbafsc@gmail.com>
2022-07-30 12:05:17 +0600

    services section design

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import bannerImg from "../../Group 1.png";
+import vector from "../../Vector.png";
 import "./Home.css"
 
 const Home = () => {
     return (
         <div className='container row'>
             <div className='col-8'>
-            <h1 className='welcomeText'>Welcome to <span>Globtech</span></h1>
-            <p className='itService'>We are committed to deliver <span>best IT services</span>. Our Consultants have experience in working with clients. We have extensive experience in the software application space and also offer a broad range and depth of technology.</p>
+            <div className='d-flex align-items-center vector'>
+                <div>
+                    <div className='line'></div>
+                    <div className='line'></div>
+                    <div className='line'></div>
+                    
+                </div>
+                <div>
+                     <img src={vector} alt="" />
+                </div>
+            </div>
+            <h1 className='welcomeText'>Welcome to <span id='globetech'>Globetech</span></h1>
+            <p className='itService'>We are committed to deliver <span id='bestIt'>best IT services</span>. Our Consultants have experience in working with clients. We have extensive experience in the software application space and also offer a broad range and depth of technology.</p>
             <div>
-                <Button>Support us</Button>
-                <Button>Our Mission</Button>
+                <Button className='me-3 supportbtn'>Support us</Button>
+                <Button className='missionbtn'>Our Mission</Button>
             </div>
             </div>
 
--- src/Component/Services/Services.js
+++ src/Component/Services/Services.js
@@ -1,18 +1,59 @@
 import React from 'react';
 import software1 from '../../software 1.png';
+import ecommerce from '../../image 3.png';
+import erp from '../../image 4.png';
+import bulksms from '../../bulk 1.png';
+import grapichs from '../../image 5.png';
+import digital from '../../image 6.png';
 import "./Services.css"
 
 const Services = () => {
     return (
         <div>
-            <h1>SERVICES</h1>
-            <h1>We provides services to our clients</h1>
+            <h3>SERVICES</h3>
+            <h3>We provides services to our clients</h3>
                {/* card */}
-            <div>
+            <div className='cardbox'>
+                {/* card1 */}
                 <div className='boxs'>
-                   <img src={software1} alt="" />
-                   <h1>Software Solution</h1>
-                   <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here',</p>
+                   <img className='imgSize' src={software1} alt="" />
+                   <h3 className='text-center textHead'>Software Solution</h3>
+                   <p className='textArea mt-4'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here',</p>
+                   <button className='buttons'>MORE</button>
+                </div>
+                {/* card2 */}
+                <div className='boxs'>
+                   <img className='imgSize' src={ecommerce} alt="" />
+                   <h3 className='text-center textHead'>E-Commerce Solution</h3>
+                   <p className='textArea mt-4'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here',</p>
+                   <button className='buttons'>MORE</button>
+                </div>
+                {/* card3 */}
+                <div className='boxs'>
+                   <img className='imgSize' src={erp} alt="" />
+                   <h3 className='text-center textHead mt-3'>ERP Solution</h3>
+                   <p className='textArea mt-4'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here',</p>
+                   <button className='buttons'>MORE</button>
+                </div>
+                {/* card4 */}
+                <div className='boxs'>
+                   <img className='imgSize' src={bulksms} alt="" />
+                   <h3 className='text-center textHead'>Bulk SMS Service</h3>
+                   <p className='textArea mt-4'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here',</p>
+                   <button className='buttons'>MORE</button>
+                </div>
+                {/* card5 */}
+                <div className='boxs'>
+                   <img className='imgSize' src={grapichs} alt="" />
+                   <h3 className='text-center textHead'>Graphic Design</h3>
+                   <p className='textArea mt-4'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here',</p>
+                   <button className='buttons'>MORE</button>
+                </div>
+                {/* card6 */}
+                <div className='boxs'>
+                   <img className='imgSize' src={digital} alt="" />
+                   <h3 className='text-center textHead'>Digital Marketing</h3>
+                   <p className='textArea mt-4'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here',</p>
                    <button className='buttons'>MORE</button>
                 </div>
             </div>
